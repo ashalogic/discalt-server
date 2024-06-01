762bcb34c66f24c34e0233086686f0aba87e49f3
Parsiya Keshavarz <ashalogic@outlook.com>
2024-06-01 03:09:17 +0200

    Port

--- src/main.ts
+++ src/main.ts
@@ -4,12 +4,11 @@ declare const module: any;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
-  2
+  await app.listen(8080);
+
   if (module.hot) {
     module.hot.accept();
     module.hot.dispose(() => app.close());
-    
   }
 }
 bootstrap();
